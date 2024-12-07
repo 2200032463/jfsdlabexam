dd62df83919350291fb88fccb83047ad02b401c0
2200032463 <141268644+2200032463@users.noreply.github.com>
2024-12-07 12:41:59 +0530

    commit project

---
+++ ENDSEMLAB/endsem/src/main/java/com/klef/exam/department.java
@@ -0,0 +1,53 @@
+package com.klef.exam;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+
+@Table(name = "department")
+public class department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int departmentId;
+
+    private String name;
+    private String location;
+    private String hodName;
+
+    public int getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(int departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getHodName() {
+        return hodName;
+    }
+
+    public void setHodName(String hodName) {
+        this.hodName = hodName;
+    }
+}
